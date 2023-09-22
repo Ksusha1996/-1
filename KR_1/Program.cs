fb54774ad4803ef75bd5c1923f8f0d486596810e
Axana <kseshechka1996@gmail.com>
2023-09-22 14:42:19 +0300

    KR_1

---
+++ KR_1/Program.cs
@@ -0,0 +1,24 @@
+﻿//написать программу, которая из имеющегося массива строк формирует новый массив из строк , длина которых меньше либо равна 3 символам .
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите массив данных через ',': ");
+        string strLine = Console.ReadLine();
+        string[] inArr = strLine.Split(",");
+        string[] outArr = new string[0];
+
+        for (int i = 0; i < inArr.Length; i++)
+        {
+            if (inArr[i].Length <= 3)
+            {
+                Array.Resize(ref outArr, outArr.Length + 1);
+                outArr[outArr.Length - 1] = inArr[i];
+            }
+
+        }
+        Console.WriteLine($"[{string.Join(",", inArr)}]=>[{string.Join(",", outArr)}]");
+    }
+}
